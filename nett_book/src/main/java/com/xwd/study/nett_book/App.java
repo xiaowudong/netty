725be8fd40cfa812ef96f69eb714e3d0d8185690
Administrator <Administrator@AFODC-611081928>
2017-05-28 23:49:59 +0800

    增加netty权威指南代码

---
+++ nett_book/src/main/java/com/xwd/study/nett_book/App.java
@@ -0,0 +1,13 @@
+package com.xwd.study.nett_book;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
